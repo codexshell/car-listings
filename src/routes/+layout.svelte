286fcdb56e8a80ac0be0341c63e17932f8b5065a
codexshell <52836290+codexshell@users.noreply.github.com>
2023-05-27 16:48:37 +0300

    feat: add global styles and normalize css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<slot />
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100;200;300;400;500;600;700;800;900&display=swap');
+	@import '../../node_modules/@csstools/normalize.css/normalize.css';
+
+	:global(:root) {
+		font-size: 62.5%;
+	}
+
+	:global(body) {
+		font-size: 1.6rem;
+		box-sizing: border-box;
+	}
+
+	:global(*, *::before, *::after) {
+		box-sizing: inherit;
+		margin: 0;
+		padding: 0;
+		font-family: 'Lexend Deca', sans-serif;
+	}
+</style>
