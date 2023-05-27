10b710a0206f7c521aa799af0e8a07b5dd1f57dc
codexshell <52836290+codexshell@users.noreply.github.com>
2023-05-27 17:07:10 +0300

    feat(iamges): add heart-filled, heart-stroke, and search SVG icons to the project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,10 @@
 	import Text from '$lib/components/Text.svelte';
 </script>
 
-<Text variant='h1' label="h1" />
-<Text variant='h2' label="h2" />
-<Text variant='p' label="p" />
-<Text variant='price' label="price" />
+<Text variant="h1" label="h1" />
+<Text variant="h2" label="h2" />
+<Text variant="p" label="p" />
+<Text variant="price" label="price" />
 
 <style>
 </style>
