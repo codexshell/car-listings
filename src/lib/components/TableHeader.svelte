14749c2d9120ecd54d2a2abce50ba45b0919fb98
codexshell <52836290+codexshell@users.noreply.github.com>
2023-05-27 20:40:17 +0300

    feat(components): add TableHeader component

---
+++ src/lib/components/TableHeader.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let headers = [];
+</script>
+
+<div class="table-header" style="--cols: {headers.length}">
+	{#each headers as header}
+		<div class="header">{header}</div>
+	{/each}
+</div>
+
+<style>
+	.table-header {
+		height: 5.5rem;
+		background-color: #008ad8;
+		display: grid;
+		grid-template-columns: repeat(var(--cols), 1fr);
+	}
+
+	.header {
+		color: white;
+		text-transform: uppercase;
+		font-weight: 400;
+		font-size: 1.4rem;
+		line-height: 1.75rem;
+		padding: 2rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script>
 	// import Text from '$lib/components/Text.svelte';
 	// import SearchInput from '$lib/components/SearchInput.svelte';
-	import CarEntry from '$lib/components/CarEntry.svelte';
+	// import CarEntry from '$lib/components/CarEntry.svelte';
+	import TableHeader from '$lib/components/TableHeader.svelte';
 
 	// const filterOptions = ['New cars', 'Used cars', 'Dealership', 'For sale by owner'];
+	const tableHeaders = ['Car details', 'Location', 'Price', 'Action'];
 </script>
 
 <main>
-	<CarEntry name="Car name" location="Car location" price="17400" />
+	<TableHeader headers={tableHeaders} />
 </main>
 
 <style>
